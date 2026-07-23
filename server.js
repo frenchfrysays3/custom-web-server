5ae37dc97c4fd982d376b9ce10a6445788ce8c82
Lucas Fry <lucas.e.fry@outlook.com>
2026-07-22 21:59:09 -0500

    feat: setup session

--- server.js
+++ server.js
@@ -8,3 +8,17 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            secure: false, // not in production
+            maxAge: 1000 * 60 * 60 * 24 // how long cookie lives: 24 hours
+        }
+    })
+);
