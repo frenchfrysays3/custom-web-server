a8d1e523a0b88aa0134d8f7b25acd202a8865854
Lucas Fry <lucas.e.fry@outlook.com>
2026-07-22 22:13:19 -0500

    feat: add root enpoint (/)

--- server.js
+++ server.js
@@ -32,3 +32,6 @@ app.use(express.json());
 // required for parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', 'index.html'));
+});
