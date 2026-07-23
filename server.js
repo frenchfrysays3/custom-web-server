1e41e2eac86b7411c19bfc39e02c10ee8dfd068a
Lucas Fry <lucas.e.fry@outlook.com>
2026-07-22 21:52:05 -0500

    feat: add very basic setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('node:path');
+const fs = require('node:fs');
+const bcrypt = require('bcrypt');
+const chalk = require('bcrypt');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
